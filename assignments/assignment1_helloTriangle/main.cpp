7fb4b66c97db40f4cdd2316ded7f60e4d7045f0c
Will Rutherford <william.rutherford@mymail.champlain.edu>
2023-09-04 15:46:20 -0400

    Started assignment 1

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,7 +8,20 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+//0,0 is the center of the window
+//1,1 is the top right
+//-1,-1 is the bottom left
+float vertices[9] = {
+	//x   //y  //z
+	-0.5, -0.5, 0.0, //Left
+	 0.5, -0.5, 0.0, //Right
+	 0.0,  0.5, 0.0  //Top 
+};
+
+unsigned int MakeVAO();
+
 int main() {
+
 	printf("Initializing...");
 	if (!glfwInit()) {
 		printf("GLFW failed to init!");
@@ -22,10 +35,12 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 
-	if (!gladLoadGL(glfwGetProcAddress)) {
+	if (!gladLoadGL(glfwGetProcAddress)) {	//Loads the GL thing, put all GL code under it
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
+
+	unsigned int vao = MakeVAO();
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -34,4 +49,27 @@ int main() {
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
+}
+
+unsigned int MakeVAO()
+{
+	//Makes new buffer ID
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocates space and sends data to GPU
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Makes a VAO that sends inputs to the vertex shader
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	//Tell vao to pull vertex data from vbo
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//sets position atributes (whatever that means)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	return vao;
 }
