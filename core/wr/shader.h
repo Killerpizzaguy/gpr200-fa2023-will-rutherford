7feff18a691aeeebbc010f3304b0ba2d758086d6
Will Rutherford <william.rutherford@mymail.champlain.edu>
2023-10-19 15:20:10 -0400

    In class work on assignment 4

--- core/wr/shader.h
+++ core/wr/shader.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <sstream>
 #include <fstream>
+#include "../ew/ewMath/mat4.h"
+#include "../ew/ewMath/vec3.h"
 #include "../ew/external/glad.h"
 
 namespace wr
@@ -18,6 +20,8 @@ namespace wr
 		void setVec2(const std::string& name, float x, float y) const;
 		void setVec3(const std::string& name, float x, float y, float z) const;
 		void setVec4(const std::string& name, float x, float y, float z, float w) const;
+
+		void setMat4(const std::string& name, const ew::Mat4& v) const;
 	private:
 		unsigned int m_id; //OpenGL program handle
 	};
